9402fa95269f4dfd5e26503be9ba8c3e0602810a
Lily Wu <20021212lilywu@gmail.com>
2023-10-16 16:14:45 +0800

    first commit

---
+++ HW2/test.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# ^ shebang(#! -> hash# + bang!): tell the system which interpreter to use
+
+# for i in `seq 1 3`; do
+#     echo i am "$USER"
+# done
+
+# var="nothing happened end closing end"
+# echo ${var}
+# echo ${#var}
+# echo ${var#*ing}
+# echo ${var##*ing}
+# echo ${var%end*}
+# echo ${var%%end*}
+
+# for i in "$*"; do
+#     echo in loop: $i
+# done
+
+# for i in "$@"; do
+#     echo in loop: $i
+# done
+
+# num1=10
+# num2=010
+# if [ $num1 = $num2 ]; then
+#     echo $num1 = $num2
+# else 
+#     echo $num1 != $num2 
+# fi
+
+# var="haha"
+
+# if [ "$var" = "hehe" ]; then
+#     echo "$var equals hehe"
+# else
+#     echo "$var doesn't equal hehe"
+# fi
+
+# a=$(($a + 9))
+# echo "$a"
+# let a=a+a
+# echo "$a"
+# a=$(expr $a + $a)
+# echo "$a"
+
