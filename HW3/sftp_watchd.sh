bee31dbc2332ddbf5d1435d5d17aee7b88db7179
Lily Wu <20021212lilywu@gmail.com>
2023-11-15 18:47:30 +0800

    11/15 18:45

--- HW3/sftp_watchd.sh
+++ HW3/sftp_watchd.sh
@@ -12,14 +12,3 @@ while true; do
     done
     sleep 0.01
 done 
-
-# while true;do
-# 	ls -tr /usr/home/sftp/public | awk '/.exe/ {print $1}'| while read line; do
-# 		p="/usr/home/sftp/public/$line"
-# 		who=$(stat -f '%Su' $p)
-#         new_path="/home/sftp/hidden/.exe/${line}"
-#         mv "$p" "$new_path"
-#         logger -p local4.info "$p violate file detected. Uploaded by ${who}."	
-# 	done
-# 	sleep 0.01 
-# done
